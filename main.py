34d734cd7d50043c0b8162945cd082f1b6c35c15
willmccloy <william.mccloy@googlemail.com>
2021-03-18 12:33:30 +0000

    Add .env and .gitignore and install python-dotenv to project

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import dotenv
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -10,9 +11,13 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, NewUserForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('SECRETKEY')
+
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
